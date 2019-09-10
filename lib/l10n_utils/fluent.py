49c7c3dd3f36085a3d7c18b2df61e102cad7060b
Paul McLanahan <pmac@mozilla.com>
2019-09-10 16:41:34 -0400

    Add fallback strings and checking for translations in template

--- lib/l10n_utils/fluent.py
+++ lib/l10n_utils/fluent.py
@@ -15,7 +15,9 @@ cache = caches['l10n']
 def _fluent_cache_key(*args):
     hash = md5(b'fluent-bundle')
     for arg in args:
-        if isinstance(arg, str):
+        if arg is None:
+            hash.update(b'None')
+        elif isinstance(arg, str):
             hash.update(force_bytes(arg))
         elif isinstance(arg, dict):
             hash.update(b':'.join([force_bytes(f'{k}={v}') for k, v in arg.items()]))
@@ -29,6 +31,9 @@ def fluent_bundle(locales, files):
     key = _fluent_cache_key(locales, files)
     bundle = cache.get(key)
     if bundle is None:
+        if isinstance(locales, str):
+            locales = [locales]
+
         # file IDs may not have file extension
         files = [f'{f}.ftl' for f in files if not f.endswith('.ftl')]
         # temporary until MultiRootLoader lands
@@ -40,13 +45,32 @@ def fluent_bundle(locales, files):
     return bundle
 
 
-def translate(string_id, files, args):
-    lang = translation.get_language(True)
-    key = _fluent_cache_key(lang, string_id, files, args)
+def has_message(message_id, bundle):
+    if not bundle._bundle_cache:
+        # need to warm the cache in the bundle
+        bundle.format_value('warm')
+
+    return bundle._bundle_cache[0].has_message(message_id)
+
+
+def has_all_messages(message_ids, files):
+    locale = translation.get_language(True)
+    bundle = fluent_bundle(locale, files)
+    return all([has_message(mid, bundle) for mid in message_ids])
+
+
+def translate(message_id, files, fallback=None, **args):
+    locale = translation.get_language(True)
+    key = _fluent_cache_key(locale, message_id, files, fallback, args)
     value = cache.get(key)
     if value is None:
-        bundle = fluent_bundle([lang, 'en'], files)
-        value = bundle.format_value(string_id, args)
+        bundle = fluent_bundle([locale, 'en'], files)
+        # check the `locale` bundle for the message if we have a fallback defined
+        if fallback and not has_message(message_id, bundle):
+            value = bundle.format_value(fallback, args)
+        else:
+            value = bundle.format_value(message_id, args)
+
         cache.set(key, value)
 
     return value
--- lib/l10n_utils/templatetags/fluent.py
+++ lib/l10n_utils/templatetags/fluent.py
@@ -5,10 +5,24 @@
 import jinja2
 from django_jinja import library
 
-from lib.l10n_utils.fluent import translate
+from lib.l10n_utils.fluent import translate, has_all_messages
 
 
 @library.global_function
 @jinja2.contextfunction
-def fluent(ctx, string_id, **args):
-    return translate(string_id, ctx['ftl_files'], args)
+def ftl(ctx, message_id, fallback=None, **args):
+    """Return the translated string.
+
+    :param ctx: the context from the template
+    :param message_id: the ID of the message
+    :param fallback: the ID of a message to use if message_id is not translated
+    :param args: the other args are passed to the translation as variables
+    """
+    return jinja2.Markup(translate(message_id, ctx['ftl_files'], fallback, **args))
+
+
+@library.global_function
+@jinja2.contextfunction
+def ftl_has_messages(ctx, *message_ids):
+    """Return True if the current translation has all of the message IDs."""
+    return has_all_messages(message_ids, ctx['ftl_files'])

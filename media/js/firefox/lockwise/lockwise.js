7c1870c0b68459a7b73063ed86f1111288661365
Errolyn <Errolyn@mozilla.com>
2019-10-07 15:44:48 -0700

    buttons only visible if javascript loads

--- media/js/firefox/lockwise/lockwise.js
+++ media/js/firefox/lockwise/lockwise.js
@@ -12,14 +12,17 @@
     else if ( !client.isFirefox ){
         document.querySelector('.for-firefox-69-and-below').remove();
         document.querySelector('.for-firefox-70-and-above').remove();
+        document.querySelector('.for-non-firefox-users').classList.remove('hidden');
     }
     else if ( client.isFirefox === true && version < '70' ) {
         document.querySelector('.for-firefox-70-and-above').remove();
         document.querySelector('.for-non-firefox-users').remove();
+        document.querySelector('.for-firefox-69-and-below').classList.remove('hidden');
     }
     else if ( client.isFirefox === true && version >= '70' ) {
         document.querySelector('.for-firefox-69-and-below').remove();
         document.querySelector('.for-non-firefox-users').remove();
+        document.querySelector('.for-firefox-70-and-above').classList.remove('hidden');
         document.querySelector('#lockwise-button').addEventListener('click', function() {
             Mozilla.UITour.showHighlight('logins');
         });
